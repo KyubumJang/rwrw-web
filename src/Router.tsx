2e9d41fd8378b0bc0d70c4d7422ed36fd2936f9c
JB Paul <jbyun94@naver.com>
2021-02-05 02:35:29 +0900

    fix: Update Home with naming convention
    겸 깃헙 테스트

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Home from './pages/HomePage';
+import HomePage from './pages/HomePage';
 
 const Router: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={HomePage} />
       <Route path="/">404 not found</Route>
     </Switch>
   );
