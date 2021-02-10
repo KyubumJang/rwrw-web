89665582c3b49d3f0160d9f0d1e9df384f03c7fd
MrMirror21 <mylittlesentry@gmail.com>
2021-02-10 18:48:25 +0900

    feat:distinguish writer from content by color

--- src/components/Letter/Letter.tsx
+++ src/components/Letter/Letter.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Text from '../Text/Text';
 import { LetterDataType } from '../../constants/Letters';
 
 export interface LetterProps extends Omit<LetterDataType, 'id'> {
@@ -17,7 +18,9 @@ const Content = styled.div``;
 const Letter: React.FC<LetterDataType> = ({ content, writer }) => {
   return (
     <Wrapper>
-      <Writer>{writer}</Writer>
+      <Text>
+        <Writer>{writer}</Writer>
+      </Text>
       <Content>{content}</Content>
     </Wrapper>
   );
--- src/components/Letter/LetterThumbnail.tsx
+++ src/components/Letter/LetterThumbnail.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { LetterDataType } from '../../constants/Letters';
+import Text from '../Text/Text';
 
 export interface LetterProps extends Omit<LetterDataType, 'content'> {
   id: string;
@@ -19,7 +20,9 @@ const LetterThumbnail: React.FC<LetterProps> = ({ thumbnail, writer, id }) => {
   const history = useHistory();
   return (
     <Wrapper onClick={() => history.push(`/letter-read/${id}`)}>
-      <Writer>{writer}</Writer>
+      <Text>
+        <Writer>{writer}</Writer>
+      </Text>
       <Thumbnail>{thumbnail}</Thumbnail>
     </Wrapper>
   );
