804188d9c8e5a8da007663dcc34a15a9549c15c0
junholee <juno7803@naver.com>
2021-02-12 18:24:02 +0900

    refactor: useInput hook Type generic으로 수정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 
-const useInput = (initValue = '') => {
-  const [value, setValue] = useState(initValue);
+const useInput = () => {
+  const [value, setValue] = useState<string | number>('');
 
   const handler = useCallback((e) => {
     setValue(e.target.value);
